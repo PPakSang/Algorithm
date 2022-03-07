f027e16391f8bcec749ddba873a149895b926b2d
PPakSang <ppm5377@naver.com>
2022-03-07 17:42:01 +0900

    PRJAVA Singleton pattern 중 lazy initialization 예제

---
+++ src/PRJAVA/singleton/lazyinit/SingletonTest.java
@@ -0,0 +1,12 @@
+package PRJAVA.singleton.lazyinit;
+
+public class SingletonTest {
+
+    public static void main(String[] args) {
+//        Singleton _a = new Singleton(); 생성 불가능
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+
+        System.out.println(a == b);
+    }
+}
